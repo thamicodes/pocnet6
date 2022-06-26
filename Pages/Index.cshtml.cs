adcc97c7712ef6926b548e54f25fef310e93691f
thmigsti <thmigs@gmail.comm>
2022-06-25 21:23:35 -0300

    teste

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeBehind.PostGreSQL.Models;
+using CodeBehind.PostGreSQL.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CodeBehind.PostGreSQL.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public IEnumerable<Pessoa> Clientes { get; set; }
+        private readonly IPessoaRepository _rep;
+
+
+        public IndexModel(ILogger<IndexModel> logger, IPessoaRepository rep)
+        {
+            _logger = logger;
+            _rep = rep;
+        }
+
+        public IActionResult OnGet()
+        {
+            _logger.LogInformation("Buscando dados");
+            Clientes = _rep.Listar();
+
+            return Page();
+        }
+
+    }
+}
---
+++ Repository/ClienteRepository.cs
@@ -0,0 +1,60 @@
+﻿//***CODE BEHIND - BY RODOLFO.FONSECA***//
+using CodeBehind.PostGreSQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeBehind.PostGreSQL.Repository
+{
+    public interface IPessoaRepository
+    {
+        Pessoa Selecionar(Guid id);
+
+        int Persistir(Pessoa cliente);
+        IEnumerable<Pessoa> Listar();
+
+        int Excluir(Guid id);
+
+        int Atualizar(Pessoa cliente);
+
+    }
+
+    public class PessoaRepository : IPessoaRepository
+    {
+
+        private readonly Contexto _contexto;
+
+        public PessoaRepository(Contexto contexto_)
+        {
+            _contexto = contexto_;
+        }
+
+        public IEnumerable<Pessoa> Listar()
+        {
+            return _contexto.Set<Pessoa>();
+        }
+
+        public int Persistir(Pessoa cliente)
+        {
+            _contexto.Clientes.Add(cliente);
+            return _contexto.SaveChanges();
+        }
+
+        public int Atualizar(Pessoa cliente)
+        {
+            _contexto.Update(cliente);
+            return _contexto.SaveChanges();
+        }
+
+        public int Excluir(Guid id)
+        {
+            _contexto.Clientes.Remove(_contexto.Clientes.Find(id));
+            return _contexto.SaveChanges();
+        }
+
+        public Pessoa Selecionar(Guid id)
+        {
+            return _contexto.Set<Pessoa>().FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
