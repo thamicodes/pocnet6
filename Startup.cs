ee41d3e96fb2064aee6dff4dbe130b9eda5367db
Thamires Silva <thamires.silva@autotrac.com.br>
2022-11-04 00:57:20 -0300

    teste

--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace CodeBehind.PostGreSQL
-{
+{ 
     public class Startup
     {
         public Startup(IConfiguration configuration)
